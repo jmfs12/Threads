8b1f87d9ee4d75925879c3e8c87e859d2893612c
Miguel123448 <155919752+Miguel123448@users.noreply.github.com>
2024-05-22 08:13:27 -0300

    Update sol.c

--- QUESTÃO 3/sol.c
+++ QUESTÃO 3/sol.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+typedef struct operation{
+
+  int ID;  // 0 - conta bancária, 1 - deposito, 2 - saque, 3 - consulta de saldo
+  int agencia;
+  int conta;
+  int saldo;  // caso seja ID 0 esse será o saldo da conta, nos demais ID usaremos como operações bancárias. 
+
+}Operation;
+
+Queue create_queue(){
+    Queue q;
+    q.front = q.rear = (Link *)malloc(sizeof(Link));
+    q.front->next = q.rear->next = NULL;
+    q.size = 0;
+    return q;
+}
+
+void enqueue(Queue *q, int item){
+    q->rear->next = create_link(item, NULL);
+    q->rear = q->rear->next;
+    q->size++;
+}
+int dequeue(Queue *q){
+    if(q->size == 0)
+        return -1;
+    else{
+        int item = q->front->next->element;
+        q->front->next = q->front->next->next;
+        if(q->front->next == NULL)
+            q->rear = q->front;
+        q->size--;
+        return item;
+    }
+}
+int frontValue(Queue q){
+    if(q.front != NULL)
+        return q.front->element;
+}
+int length(Queue q){
+    if(q.size > 0)
+        return q.size;
+}
