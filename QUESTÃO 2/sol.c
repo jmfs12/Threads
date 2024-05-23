16130998e483df5299677a1046f4756de3d5d1b9
João Miguel Francisco de Souza <144076975+jmfs12@users.noreply.github.com>
2024-05-23 08:49:44 -0300

    Update sol.c

--- QUESTÃO 2/sol.c
+++ QUESTÃO 2/sol.c
@@ -1,48 +1,110 @@
-/*2. Crie um programa em C que utilize o algoritmo de ordenação bubble sort de maneira concorrente. 
-O programa deve dividir um array em N partes, e cada thread deve ordenar uma parte. Após todas as 
-threads completarem a ordenação de suas respectivas partes, uma thread final deve mesclar todos os
-segmentos para formar o array ordenado completo. Utilize barriers para sincronizar a conclusão das
-ordenações parciais antes de começar a mesclagem.
-*/
+#define _XOPEN_SOURCE 600
+
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-void* bubbleSort(int array[], int tamanho);
+pthread_barrier_t barrier;
 
-pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+int len;
+int parts;
+int *vet;
+int *vetFim;
 
-int main(){
-
-    int tam;
-    int n;
-    printf("Indique o tamanho do array para ser gerado de forma aleatória: ");
-    scanf("%d",&tam);
-    int array[tam];
-    printf("O array gerado, em ordem é: "\n);
-    for (int i = 0; i < tam; i++) {
-    array[i] = rand() % 100;
-    printf("%d ",array[i]);
+void *bubble(void *threadid){
+    int tid = *((int *)threadid);
+    int temp = 0;
+    for(int i = tid * len/parts;i<(tid+1)*len/parts;i++){
+      for(int j = tid * len/parts;j<(tid+1)*len/parts;j++){
+        if(vet[i]<vet[j]){
+          temp = vet[i];
+          vet[i] = vet[j];
+          vet[j] = temp;
+        }
+      }
     }
-    printf("Digite a quantidade de bubblesorts concorrente: ");
-    scanf("%d",&n);
-    //alocação dos threads
-    pthread_t *threads = (pthread_t *)malloc(n * sizeof(pthread_t));
-    if(threads == NULL) { printf("Erro na alocacao1\n"); exit(1); };
-    int **taskids = (int **)malloc(n * sizeof(int*));
-    if(taskids == NULL) { printf("Erro na alocacao2\n"); free(threads); exit(1); };
-    //criação dos threads
-    int rc;
-    
+    pthread_barrier_wait(&barrier);
+    pthread_exit(NULL);
 }
 
-void* bubbleSort(int array[], int tamanho) {
-    for (int i = 0; i < tamanho - 1; i++) {
-        for (int j = 0; j < tamanho - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
+void *Final(void *threadid) {
+    //int *tid = (int *)threadid;
+    int vetInd[parts];//Onde ficará armazenado onde começa cada sub-array
+    for(int i = 0;i<parts;i++){
+        vetInd[i] = len / parts * i;//botando onde cada thread começa
+    }
+    int indice;//onde será armazenado o índice
+    int temp = 0;
+    for (int i = 0; i < len; i++) {
+        int menor = __INT_MAX__;
+        while (temp < parts) {
+            //pegar o menor de todos
+            if(vet[vetInd[temp]] < menor){
+                indice = temp; //coletamos o indice para incrementar o vetor de indices responsavel por dizer quem é o proximo numero a ser comparado do subarray
+                menor = vet[vetInd[temp]];
             }
+                temp++;
+        }
+        
+        vet[vetInd[indice]] = __INT_MAX__; // no lugar do numero adicionado vai o max inteirp
+        vetFim[i]= menor; // o menor vai no vetor final
+        vetInd[indice]++; //incrementa o valor do vetor de indices para que ele compare com o próximo número do seu subarray
+        temp = 0;
+        if(vetInd[parts-1] == len){//caso o ultimo "subarray" tenha sido posto por completo, para evitar que ele compare com a invasão de memória
+            vetInd[parts-1]= (len / parts) * (parts-1);//colocamos o vetor de índice para comparar com o primeiro elemento do "subarray"
         }
     }
+  pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[]){
+    printf("Insira a quantidade de partes para dividir o array: ");
+    scanf("%d", &parts);
+
+    printf("\nIndique o tamanho do array para ser gerado de forma aleatória: ");
+    scanf("%d", &len);
+
+    vet = (int*)malloc(len*sizeof(int));
+    vetFim = (int*)malloc(len*sizeof(int));
+
+    printf("\nO array gerado, em ordem é: \n");
+    srand(time(NULL));
+    for (int i = 0; i < len; i++) {
+        vet[i] = rand() % 100;
+        printf("%d ",vet[i]);
+    }
+    
+    pthread_t threads[parts];
+    int *taskids[parts];
+
+    pthread_barrier_init(&barrier, NULL, parts+1); //iniciando a barreira para esperar as threads definidas + a main
+    int rc; int t; int u;
+    for(t=0; t<parts; t++){
+      printf("\nNo main: criando thread %d", t);
+      taskids[t] = (int *) malloc(sizeof(int));
+      *taskids[t] = t;
+      rc = pthread_create(&threads[t],NULL,bubble, (void *)taskids[t]);
+      if(rc){//Conferindo se as threads estão sendo criadas.
+        printf("NÃO FOI POSSÍVEL CRIAR A THREAD %d", t);
+        exit(1);
+      }
+    }
+
+    pthread_barrier_wait(&barrier); // quanto todos chegarem, incluindo a main, todos passam
+
+    pthread_t threads2;// inicializando a thread final
+    rc = pthread_create(&threads2,NULL,Final, NULL); // criando a thread e passando ela pra função que vai juntar
+    if(rc){// conferindo se a thread foi criada 
+      printf("NÃO FOI POSSÍVEL CRIAR A THREAD FINAL\n");
+      exit(1);
+    }
+    pthread_join(threads2, NULL); // espernado a thread chegar para prosseguir
+    for (int i = 0; i < len; i++) {
+        printf("%d ", vetFim[i]);
+    }
+    printf("\n");
+    pthread_barrier_destroy(&barrier); // destruindo a barreira
+    pthread_exit(NULL); // finalizando programa
 }
