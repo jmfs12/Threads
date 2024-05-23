a024796528c2f5c952c7319e4bc1a9689dad06b8
Miguel123448 <155919752+Miguel123448@users.noreply.github.com>
2024-05-23 17:10:54 -0300

    Update 3_sol.c

--- QUESTÃO 3/3_sol.c
+++ QUESTÃO 3/3_sol.c
@@ -11,9 +11,9 @@ Operation dequeue(Queue* q);
 
 typedef struct operation{
 
-  int operacao;  // 0 - Armazenando informações da conta bancária, 1 - saldo, 2 - deposito, 3 - saque
+  int operacao;  //-1 - Operação falha, 0 - Armazenando informações da conta bancária, 1 - saldo, 2 - deposito, 3 - saque
   int conta;
-  int saldo;  // caso seja ID 0 esse será o saldo da conta, nos demais ID usaremos como operações bancárias. 
+  float saldo;  // caso seja ID 0 esse será o saldo da conta, nos demais ID usaremos como operações bancárias. 
   int threadID; //identificação do thread que a solicitou
 
 }Operation;
@@ -32,8 +32,10 @@ typedef struct queue{
     Queue* buffer;          //buffer do cliente para o banco
     Queue* buffer_output;   //buffer da respsota do banco para o cliente
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t* resposta;
     pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+    pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
+    pthread_cond_t* cnd_resposta;
 
 int main(){
     int N,rc;
@@ -51,14 +53,23 @@ int main(){
     if(taskids==NULL) {printf("Erro na alocação 2.");exit(2);}
     pthread_t* banco = (pthread_t*) malloc(sizeof(pthread_t));
     if(banco==NULL) {printf("Erro na alocação 3.");exit(2);}
+    resposta = (pthread_mutex_t *) malloc(N*sizeof(pthread_mutex_t));
+    if(resposta==NULL) {printf("Erro na alocação 4.");exit(2);};
+    cnd_resposta = (pthread_cond_t *) malloc(N*sizeof(pthread_cond_t));
+    if(cnd_resposta==NULL) {printf("Erro na alocação 5.");exit(2);};
 
     //Criação propriamente dita das threads, utilizamos rc para identificar qualquer possível erro na criação
+
     for(int i=0;i<N;i++){
         taskids[i] = (int *)malloc(sizeof(int));
-        if(taskids[i]==NULL) {printf("Erro na alocação 4.");exit(2);}
+        if(taskids[i]==NULL) {printf("Erro na alocação 6.");exit(3);}
         *taskids[i]=i;
         rc = pthread_create(&clientes[i],NULL,fclientes,(void *) taskids[i]);
         if(rc) {printf("Erro na alocação do thread 1.");exit(3);}
+        rc = pthread_cond_init(&cnd_resposta[i],NULL);
+        if(rc) {printf("Erro na alocacao da condição 1.");exit(3);};
+        rc = pthread_mutex_init(&resposta[i],NULL);
+        if(rc) {printf("Erro na alocacao do mutex 1.");exit(3);};
     }
     rc = pthread_create(&banco,NULL,fbanco,NULL);
     if(rc) {printf("Erro na alocação do thread 2.");exit(3);}
@@ -66,19 +77,73 @@ int main(){
     
 }
 
+Operation put_wait(Operation op,int threadid){
+    //para inserir no buffer, bloqueamos que os demais consumidores façam o mesmo para evitar problemas
+    pthread_mutex_lock(&mutex);
+    if(buffer->size==BUFFER_SIZE){
+        printf("Servidor cheio, aguarde um momento.\n");
+        while(buffer->size==BUFFER_SIZE){
+            pthread_cond_wait(&empty,&mutex);
+        }
+    }
+    enqueue(buffer,op);//inserindo no buffer
+    if(buffer->size==1){
+        pthread_cond_signal(&fill);
+    }
+    pthread_mutex_unlock(&mutex);
+    //entregamos a solicitacao no buffer para o banco, agora temos que aguardar a resposta
+    //Não podemos compartilhar o mutex entre os clientes, já que bloquearia todos ao mesmo tempo
+    //logo, vamos ter que utilizar um mutex unico para cada cliente para que ele aguarde a respsota do banco
+    //o unlock é feito pelo banco.
+    pthread_mutex_lock(&resposta[threadid]);
+    return dequeue(buffer_output);
+}
+
 void fclientes(int taskid){   //agindo como produtor
-    int op=0;
+    int op=0,conta;
+    float valor;
+        printf("Olá cliente %d, informe sua conta: ",taskid);
+        scanf("%d",&conta);
     while(op!=4){
-        printf("Olá cliente %d, digite a operação que deseja realizar. \n",taskid+1);
+        printf("(Cliente %d) Digite a operação que deseja realizar na conta %d \n",taskid,conta);
         printf("1. Saldo da conta\n2.Deposito\n3.Saque\n4. Sair\n");
         scanf("%d",&op);
-        if(op==1)
+        Operation escolha;
+        Operation resposta;
+        escolha.conta=conta;
+        escolha.operacao=op;
+        escolha.threadID=taskid;    //Estamos identificando o thread que solicitou a operação, para que o banco o acorde quando devolver o solicitação
+        if(op==1){
+            resposta = put_wait(escolha,taskid);
+            printf("O saldo da conta %d é de: %d \n",resposta.conta,resposta.saldo);
+        }else if(op==2){
+            printf("Digite o valor para deposito: ");
+            scanf("%d",&valor);
+            escolha.saldo=valor;
+            resposta = put_wait(escolha,taskid);
+            printf("Deposito realizado com sucesso, o novo saldo da conta %d é de: %d\n",resposta.conta,resposta.saldo);
+        }else if(op==3){
+            printf("Digite o valor para saque: ");
+            scanf("%d",&valor);
+            escolha.saldo=valor;
+            resposta = put_wait(escolha,taskid);
+            if(resposta.operacao==-1){  //Se o banco retornou com uma operacao -1, significa que nao ha saldo suficiente
+                printf("O saldo da conta é insuficiente para realizar o saque solicitado.\n");
+                printf("O saldo é de: %d\n",resposta.saldo);
+            }else{
+                printf("Saque realizado com sucesso, o novo saldo é de: %d\n",resposta.saldo);
+            }
+        }else if(op==4){
+            printf("Encerrando a sessão.\n");
+            break;
+        }else printf("Opção inválida, tente novamente.\n");
     }
 
 }
 
 void fbanco(){  //agindo como consumidor
 
+    
 }
 
 
@@ -108,7 +173,7 @@ void enqueue(Queue* q, Operation it){
     q->size++;
 }
 Operation dequeue(Queue* q){
-    if(q->size==0) return 0;
+    if(q->size==0) return NULL;
     else{
         Operation it=q->front->next->element;
         q->front->next=q->front->next->next;
